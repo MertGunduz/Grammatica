9e8157ae111f7d46eee5fe2c082b149130a99e72
Mert Gunduz <merttgg@gmail.com>
2021-11-03 15:54:40 +0300

    Coloring & Object Naming

--- Forms/Grammatica_AwakeForm.Designer.cs
+++ Forms/Grammatica_AwakeForm.Designer.cs
@@ -30,15 +30,33 @@ namespace Grammatica.Forms
         private void InitializeComponent()
         {
             this.Main_Panel = new System.Windows.Forms.Panel();
-            this.TopBorder_Panel = new System.Windows.Forms.Panel();
-            this.BottomBorder_Panel = new System.Windows.Forms.Panel();
-            this.RightBorder_Panel = new System.Windows.Forms.Panel();
+            this.InnerMain_Panel = new System.Windows.Forms.Panel();
+            this.InnerLeftBorder_Panel = new System.Windows.Forms.Panel();
+            this.InnerRightBorder_Panel = new System.Windows.Forms.Panel();
+            this.InnerBottomBorder_Panel = new System.Windows.Forms.Panel();
+            this.InnerTopBorder_Panel = new System.Windows.Forms.Panel();
+            this.BottomMargin_Panel = new System.Windows.Forms.Panel();
+            this.TopMargin_Panel = new System.Windows.Forms.Panel();
+            this.LeftMargin_Panel = new System.Windows.Forms.Panel();
+            this.RightMargin_Panel = new System.Windows.Forms.Panel();
             this.LeftBorder_Panel = new System.Windows.Forms.Panel();
+            this.RightBorder_Panel = new System.Windows.Forms.Panel();
+            this.BottomBorder_Panel = new System.Windows.Forms.Panel();
+            this.TopBorder_Panel = new System.Windows.Forms.Panel();
             this.Main_Panel.SuspendLayout();
             this.SuspendLayout();
             // 
             // Main_Panel
             // 
+            this.Main_Panel.Controls.Add(this.InnerMain_Panel);
+            this.Main_Panel.Controls.Add(this.InnerLeftBorder_Panel);
+            this.Main_Panel.Controls.Add(this.InnerRightBorder_Panel);
+            this.Main_Panel.Controls.Add(this.InnerBottomBorder_Panel);
+            this.Main_Panel.Controls.Add(this.InnerTopBorder_Panel);
+            this.Main_Panel.Controls.Add(this.BottomMargin_Panel);
+            this.Main_Panel.Controls.Add(this.TopMargin_Panel);
+            this.Main_Panel.Controls.Add(this.LeftMargin_Panel);
+            this.Main_Panel.Controls.Add(this.RightMargin_Panel);
             this.Main_Panel.Controls.Add(this.LeftBorder_Panel);
             this.Main_Panel.Controls.Add(this.RightBorder_Panel);
             this.Main_Panel.Controls.Add(this.BottomBorder_Panel);
@@ -49,23 +67,95 @@ namespace Grammatica.Forms
             this.Main_Panel.Size = new System.Drawing.Size(400, 400);
             this.Main_Panel.TabIndex = 0;
             // 
-            // TopBorder_Panel
+            // InnerMain_Panel
             // 
-            this.TopBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(180)))), ((int)(((byte)(253)))));
-            this.TopBorder_Panel.Dock = System.Windows.Forms.DockStyle.Top;
-            this.TopBorder_Panel.Location = new System.Drawing.Point(0, 0);
-            this.TopBorder_Panel.Name = "TopBorder_Panel";
-            this.TopBorder_Panel.Size = new System.Drawing.Size(400, 2);
-            this.TopBorder_Panel.TabIndex = 0;
+            this.InnerMain_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(45)))), ((int)(((byte)(129)))));
+            this.InnerMain_Panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.InnerMain_Panel.Location = new System.Drawing.Point(44, 44);
+            this.InnerMain_Panel.Name = "InnerMain_Panel";
+            this.InnerMain_Panel.Size = new System.Drawing.Size(312, 312);
+            this.InnerMain_Panel.TabIndex = 12;
             // 
-            // BottomBorder_Panel
+            // InnerLeftBorder_Panel
             // 
-            this.BottomBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(180)))), ((int)(((byte)(253)))));
-            this.BottomBorder_Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.BottomBorder_Panel.Location = new System.Drawing.Point(0, 398);
-            this.BottomBorder_Panel.Name = "BottomBorder_Panel";
-            this.BottomBorder_Panel.Size = new System.Drawing.Size(400, 2);
-            this.BottomBorder_Panel.TabIndex = 1;
+            this.InnerLeftBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(180)))), ((int)(((byte)(253)))));
+            this.InnerLeftBorder_Panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.InnerLeftBorder_Panel.Location = new System.Drawing.Point(42, 44);
+            this.InnerLeftBorder_Panel.Name = "InnerLeftBorder_Panel";
+            this.InnerLeftBorder_Panel.Size = new System.Drawing.Size(2, 312);
+            this.InnerLeftBorder_Panel.TabIndex = 11;
+            // 
+            // InnerRightBorder_Panel
+            // 
+            this.InnerRightBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(180)))), ((int)(((byte)(253)))));
+            this.InnerRightBorder_Panel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.InnerRightBorder_Panel.Location = new System.Drawing.Point(356, 44);
+            this.InnerRightBorder_Panel.Name = "InnerRightBorder_Panel";
+            this.InnerRightBorder_Panel.Size = new System.Drawing.Size(2, 312);
+            this.InnerRightBorder_Panel.TabIndex = 10;
+            // 
+            // InnerBottomBorder_Panel
+            // 
+            this.InnerBottomBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(180)))), ((int)(((byte)(253)))));
+            this.InnerBottomBorder_Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.InnerBottomBorder_Panel.Location = new System.Drawing.Point(42, 356);
+            this.InnerBottomBorder_Panel.Name = "InnerBottomBorder_Panel";
+            this.InnerBottomBorder_Panel.Size = new System.Drawing.Size(316, 2);
+            this.InnerBottomBorder_Panel.TabIndex = 9;
+            // 
+            // InnerTopBorder_Panel
+            // 
+            this.InnerTopBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(180)))), ((int)(((byte)(253)))));
+            this.InnerTopBorder_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.InnerTopBorder_Panel.Location = new System.Drawing.Point(42, 42);
+            this.InnerTopBorder_Panel.Name = "InnerTopBorder_Panel";
+            this.InnerTopBorder_Panel.Size = new System.Drawing.Size(316, 2);
+            this.InnerTopBorder_Panel.TabIndex = 8;
+            // 
+            // BottomMargin_Panel
+            // 
+            this.BottomMargin_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(56)))), ((int)(((byte)(202)))));
+            this.BottomMargin_Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomMargin_Panel.Location = new System.Drawing.Point(42, 358);
+            this.BottomMargin_Panel.Name = "BottomMargin_Panel";
+            this.BottomMargin_Panel.Size = new System.Drawing.Size(316, 40);
+            this.BottomMargin_Panel.TabIndex = 7;
+            // 
+            // TopMargin_Panel
+            // 
+            this.TopMargin_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(56)))), ((int)(((byte)(202)))));
+            this.TopMargin_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopMargin_Panel.Location = new System.Drawing.Point(42, 2);
+            this.TopMargin_Panel.Name = "TopMargin_Panel";
+            this.TopMargin_Panel.Size = new System.Drawing.Size(316, 40);
+            this.TopMargin_Panel.TabIndex = 6;
+            // 
+            // LeftMargin_Panel
+            // 
+            this.LeftMargin_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(56)))), ((int)(((byte)(202)))));
+            this.LeftMargin_Panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftMargin_Panel.Location = new System.Drawing.Point(2, 2);
+            this.LeftMargin_Panel.Name = "LeftMargin_Panel";
+            this.LeftMargin_Panel.Size = new System.Drawing.Size(40, 396);
+            this.LeftMargin_Panel.TabIndex = 5;
+            // 
+            // RightMargin_Panel
+            // 
+            this.RightMargin_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(56)))), ((int)(((byte)(202)))));
+            this.RightMargin_Panel.Dock = System.Windows.Forms.DockStyle.Right;
+            this.RightMargin_Panel.Location = new System.Drawing.Point(358, 2);
+            this.RightMargin_Panel.Name = "RightMargin_Panel";
+            this.RightMargin_Panel.Size = new System.Drawing.Size(40, 396);
+            this.RightMargin_Panel.TabIndex = 4;
+            // 
+            // LeftBorder_Panel
+            // 
+            this.LeftBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(180)))), ((int)(((byte)(253)))));
+            this.LeftBorder_Panel.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftBorder_Panel.Location = new System.Drawing.Point(0, 2);
+            this.LeftBorder_Panel.Name = "LeftBorder_Panel";
+            this.LeftBorder_Panel.Size = new System.Drawing.Size(2, 396);
+            this.LeftBorder_Panel.TabIndex = 3;
             // 
             // RightBorder_Panel
             // 
@@ -76,14 +166,23 @@ namespace Grammatica.Forms
             this.RightBorder_Panel.Size = new System.Drawing.Size(2, 396);
             this.RightBorder_Panel.TabIndex = 2;
             // 
-            // LeftBorder_Panel
+            // BottomBorder_Panel
             // 
-            this.LeftBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(180)))), ((int)(((byte)(253)))));
-            this.LeftBorder_Panel.Dock = System.Windows.Forms.DockStyle.Left;
-            this.LeftBorder_Panel.Location = new System.Drawing.Point(0, 2);
-            this.LeftBorder_Panel.Name = "LeftBorder_Panel";
-            this.LeftBorder_Panel.Size = new System.Drawing.Size(2, 396);
-            this.LeftBorder_Panel.TabIndex = 3;
+            this.BottomBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(180)))), ((int)(((byte)(253)))));
+            this.BottomBorder_Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.BottomBorder_Panel.Location = new System.Drawing.Point(0, 398);
+            this.BottomBorder_Panel.Name = "BottomBorder_Panel";
+            this.BottomBorder_Panel.Size = new System.Drawing.Size(400, 2);
+            this.BottomBorder_Panel.TabIndex = 1;
+            // 
+            // TopBorder_Panel
+            // 
+            this.TopBorder_Panel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(180)))), ((int)(((byte)(253)))));
+            this.TopBorder_Panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopBorder_Panel.Location = new System.Drawing.Point(0, 0);
+            this.TopBorder_Panel.Name = "TopBorder_Panel";
+            this.TopBorder_Panel.Size = new System.Drawing.Size(400, 2);
+            this.TopBorder_Panel.TabIndex = 0;
             // 
             // Grammatica_AwakeForm
             // 
@@ -107,5 +206,14 @@ namespace Grammatica.Forms
         private System.Windows.Forms.Panel RightBorder_Panel;
         private System.Windows.Forms.Panel BottomBorder_Panel;
         private System.Windows.Forms.Panel TopBorder_Panel;
+        private System.Windows.Forms.Panel BottomMargin_Panel;
+        private System.Windows.Forms.Panel TopMargin_Panel;
+        private System.Windows.Forms.Panel LeftMargin_Panel;
+        private System.Windows.Forms.Panel RightMargin_Panel;
+        private System.Windows.Forms.Panel InnerLeftBorder_Panel;
+        private System.Windows.Forms.Panel InnerRightBorder_Panel;
+        private System.Windows.Forms.Panel InnerBottomBorder_Panel;
+        private System.Windows.Forms.Panel InnerTopBorder_Panel;
+        private System.Windows.Forms.Panel InnerMain_Panel;
     }
 }
