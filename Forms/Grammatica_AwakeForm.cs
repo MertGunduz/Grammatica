1eae1c99154048b9f1fd8870b13cbbf9d1123274
Mert Gunduz <merttgg@gmail.com>
2021-11-03 15:40:07 +0300

    Grammatica Source Added 😋

---
+++ Forms/Grammatica_AwakeForm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Grammatica.Forms
+{
+    public partial class Grammatica_AwakeForm : Form
+    {
+        public Grammatica_AwakeForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
