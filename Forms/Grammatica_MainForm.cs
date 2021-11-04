f69c14e0cd58735d25e8d6a615efd106c9fe24e9
Mert Gunduz <merttgg@gmail.com>
2021-11-05 02:13:47 +0300

    Code Cleaned & Fixed

--- Forms/Grammatica_MainForm.cs
+++ Forms/Grammatica_MainForm.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Text;
 using System.Data.SqlClient;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace Grammatica.Forms
